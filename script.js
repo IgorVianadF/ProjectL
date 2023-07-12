1e815e53f2e3f117e71a92a62803e4f3abd30b2c
IgorVianadF <igorv02004@gmail.com>
2023-07-12 17:23:34 -0300

    adicionado JS, mudança de logo e icon

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var ooo = document.getElementsByClassName("out-of-order");
+
+  for (var i = 0; i < ooo.length; i++) {
+    ooo[i].addEventListener("click", function() {
+      alert("Not implemented yet");
+    });
+  }
+});
